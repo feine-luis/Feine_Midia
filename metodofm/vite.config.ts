d515610729b7a482e811ce3c2ef1598e61c2e6ae
feine-luis <131889816+feine-luis@users.noreply.github.com>
2026-08-25 22:04:33 -0300

    Update vite.config.ts

--- metodofm/vite.config.ts
+++ metodofm/vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig(({ mode }) => {
         "@": path.resolve(__dirname, "./src"),
       },
     },
-    base: '/',
+    base: '/metodofm/',
     build: {
       outDir: 'dist',
     }
